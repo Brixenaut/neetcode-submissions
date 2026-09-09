3d0cc3c2344c39173c4a3b5508d61192e9a98df3
Brixenaut <152750461+Brixenaut@users.noreply.github.com>
2026-09-09 16:08:53 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+        int[] counter = new int[26];
+        for (int i = 0; i < s.Length; i++){
+            counter[s[i] - 'a'] ++;
+            counter[t[i] - 'a'] --;
+        }
+        foreach(int i in counter){
+            if (i != 0) return false;
+        }
+        return true;
+    }
+}
