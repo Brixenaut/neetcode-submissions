1b3f300ec2ab6f83615049573d9ca1ea10b0b767
Brixenaut <152750461+Brixenaut@users.noreply.github.com>
2026-09-09 15:31:45 -0500

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        int numLen = nums.Length;
+        if (numLen <= 1) return false;
+        HashSet<int> dupeCheck = new HashSet<int>(numLen);
+        foreach (int i in nums) {
+            if (!dupeCheck.Add(i)) return true;
+        }
+        return false;
+    }
+}
