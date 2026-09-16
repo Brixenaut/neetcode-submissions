7535370387f3d93c44d246292e148073e09ec637
Brixenaut <152750461+Brixenaut@users.noreply.github.com>
2026-09-16 17:12:28 -0500

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        Dictionary<string, HashSet<char>> squares = new()
+        {
+            {"00", new()},
+            {"01", new()},
+            {"02", new()},
+            {"10", new()},
+            {"11", new()},
+            {"12", new()},
+            {"20", new()},
+            {"21", new()},
+            {"22", new()}
+        };
+        for (int i = 0; i < board.Length; i++){
+            HashSet<char> columns = new();
+            HashSet<char> rows = new();
+            for (int j = 0; j < board[0].Length; j++){
+                char symbolRow = board[i][j];
+                char symbolCol = board[j][i];
+                int x = i / 3;
+                int y = j / 3;
+                string sq = $"{x}{y}";
+
+                if (!(symbolRow == '.')){
+                    if (!rows.Add(board[i][j]) || !squares[sq].Add(board[i][j])){
+                        return false;
+                    }
+                }
+                if  (!(symbolCol == '.')){
+                    if (!columns.Add(board[j][i])){
+                        return false;                    
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
